ce2020a4bb38d373db6f2ed8b13898e65243880b
Harish Rohini <harish.hari31@gmail.com>
2015-11-02 13:30:31 -0600

    Initial Commit

---
+++ malloctrace/malloctrace.py
@@ -0,0 +1,19 @@
+__author__ = 'HarishRohini'
+
+class Malloctrace(object):
+    """Returns a list containing the tuples of address ranges"""
+    def __init__(self, filename):
+        super(Malloctrace, self).__init__()
+        self.filename = filename
+        self.address_range_list = []
+
+    def process_file():
+        pass
+
+    def write_to_file():
+        pass
+
+
+if __name__ == '__main__':
+    malloctrace = Malloctrace("malloctrace.out")
+    print malloctrace.filename
---
+++ memorytrace/memorytrace.py
@@ -0,0 +1,18 @@
+__author__ = 'HarishRohini'
+
+class MemoryTrace(object):
+    """Take the mem trace file and check if the R/W address exists in the allocated range from Malloctrace"""
+    def __init__(self, filename):
+        super(MemoryTrace, self).__init__()
+        self.filename = filename
+
+    def process_file():
+        pass
+
+    def write_to_file():
+        pass
+
+
+if __name__ == '__main__':
+    memorytrace = MemoryTrace("memorytrace.out")
+    print memorytrace.filename
