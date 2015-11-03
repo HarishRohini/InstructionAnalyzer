75cc75f26dcdc0d5ae8686858238f09bbb952bc4
Harish Rohini <harish.hari31@gmail.com>
2015-11-02 22:25:24 -0600

    Parse the malloctrace file

--- malloctrace/malloctrace.py
+++ malloctrace/malloctrace.py
@@ -9,8 +9,16 @@ class Malloctrace(object):
         self.filename = filename
         self.address_range_list = []
 
-    def process_file():
-        pass
+    def process_file(self):
+        number_of_bytes, start_address = None, None
+        with open(self.filename, 'r') as f:
+            temp = f.readline()
+            for line in f:
+                if line.find('returns') != -1:
+                    number_of_bytes, start_address = temp[7:-2], line[10:-1]
+                    print (number_of_bytes,start_address)
+                else:
+                    temp = line
 
     def write_to_file():
         pass
@@ -18,9 +26,12 @@ class Malloctrace(object):
 
 if __name__ == '__main__':
     parser = optparse.OptionParser()
-    parser.add_option('-f','--file',help="malloctrace.out file", dest='malloctrace_file', action='store_true')
+    parser.add_option('-f','--file',help="malloctrace.out file", dest='malloctrace_file', action='store', type='string')
     (opts, args) = parser.parse_args()
 
     if opts.malloctrace_file is None:
         parser.print_help()
         exit(-1)
+
+    malloctrace = Malloctrace(opts.malloctrace_file)
+    malloctrace.process_file()
